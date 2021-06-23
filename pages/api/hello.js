3c40062009d6c8803ee68428b4fb6681b89347c9
wangshishuo <wangshishuo@gmail.com>
2021-06-23 10:28:53 +0000

    add package for api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const _ = require('lodash');
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  res.statusCode = 200;
+  let c = _.defaults({ 'a': 1 }, { 'a': 3, 'b': 2 });
+  res.json({ name: JSON.stringify(c) });
 }
