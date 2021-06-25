20d07514fbacb402dfbdd6b05758ac2b48e3ed0e
wangshishuo <wangshishuo@gmail.com>
2021-06-25 05:47:26 +0000

    test vercel-build

--- api/hello.js
+++ api/hello.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 const { join } = require('path')
-// const { grayscale } = require('./grayscale_lib.js');
+const { grayscale } = require('./grayscale_lib.js');
 
 module.exports = (req, res) => {
   res.setHeader('content-type', 'text/plain')
 
   var img_src = fs.readFileSync(join(__dirname, 'cowboy.png'));
-  // fs.writeFileSync(join(__dirname, 'gray.png'), grayscale(img_src));
+  fs.writeFileSync(join(__dirname, 'gray.png'), grayscale(img_src));
 
   let x = fs.existsSync(join(__dirname, 'gray.png'))
   res.send(`${__dirname} ${x}`)
