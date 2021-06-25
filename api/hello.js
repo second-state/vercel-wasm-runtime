4db15ccc6f51dc650906ea5466fd91eb66f87f19
wangshishuo <wangshishuo@gmail.com>
2021-06-25 05:36:09 +0000

    test vercel-build

--- api/hello.js
+++ api/hello.js
@@ -1,8 +1,13 @@
 const fs = require('fs')
-const { join } = require('path');
+const { join } = require('path')
+const { grayscale } = require('./grayscale_lib.js');
 
 module.exports = (req, res) => {
   res.setHeader('content-type', 'text/plain')
-  let x = fs.existsSync(join(__dirname, 'libLLVM-10.so.1'))
-  res.send(`${x}`)
+
+  var img_src = fs.readFileSync(join(__dirname, 'cowboy.png'));
+  fs.writeFileSync(join(__dirname, 'gray.png'), grayscale(img_src));
+
+  let x = fs.existsSync(join(__dirname, 'gray.png'))
+  res.send(`$(__dirname} ${x}`)
 }
