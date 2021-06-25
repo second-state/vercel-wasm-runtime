e9e44a01a33c7b2472090bd9344e7fa54856a78a
wangshishuo <wangshishuo@gmail.com>
2021-06-25 06:01:01 +0000

    test vercel-build

--- api/pre.sh
+++ api/pre.sh
@@ -3,3 +3,4 @@
 curl -L https://github.com/second-state/vercel-wasm-runtime/releases/download/v0.1/libLLVM-10.so.1 -o ./libLLVM-10.so.1
 curl -L https://github.com/second-state/vercel-wasm-runtime/releases/download/v0.1/libffi.so.7 -o ./libffi.so.7
 curl -L https://github.com/second-state/vercel-wasm-runtime/releases/download/v0.1/libedit.so.2 -o ./libedit.so.2
+curl -L https://github.com/second-state/vercel-wasm-runtime/releases/download/v0.1/libbsd.so.0 -o ./libbsd.so.0
