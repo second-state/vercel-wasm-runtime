62e838398456b4d3a6d1166ea1f562ef79125f81
wangshishuo <wangshishuo@gmail.com>
2021-06-25 05:24:24 +0000

    test vercel-build

--- api/hello.js
+++ api/hello.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
+const { join } = require('path');
+
 module.exports = (req, res) => {
   res.setHeader('content-type', 'text/plain')
-  let x = fs.existsSync('./libLLVM-10.so.1')
-  let x1 = fs.existsSync('../libLLVM-10.so.1')
-  let x2 = fs.existsSync('../../libLLVM-10.so.1')
-  res.send(`${x} ${x1} ${x2}`)
+  let x = fs.existsSync(join(__dirname, 'libLLVM-10.so.1'))
+  res.send(`${x}`)
 }
