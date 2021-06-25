23be81a9f842a889593e2943440f3d85e9348910
wangshishuo <wangshishuo@gmail.com>
2021-06-25 10:05:22 +0000

    test vercel-build with musl-linux-lib

--- api/pre.sh
+++ api/pre.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-curl -L https://github.com/second-state/vercel-wasm-runtime/releases/download/v0.1/libLLVM-10.so.1 -o ./libLLVM-10.so.1
-curl -L https://github.com/second-state/vercel-wasm-runtime/releases/download/v0.1/libffi.so.7 -o ./libffi.so.7
-curl -L https://github.com/second-state/vercel-wasm-runtime/releases/download/v0.1/libedit.so.2 -o ./libedit.so.2
-curl -L https://github.com/second-state/vercel-wasm-runtime/releases/download/v0.1/libbsd.so.0 -o ./libbsd.so.0
-curl -L https://github.com/second-state/vercel-wasm-runtime/releases/download/v0.1/libstdc++.so.6 -o ./libstdc++.so.6
-curl -L https://github.com/second-state/vercel-wasm-runtime/releases/download/v0.1/libc.so.6 -o ./libc.so.6
+curl -L https://github.com/second-state/vercel-wasm-runtime/releases/download/v0.1/musl-linux-lib.tar.gz -o ./musl-linux-lib.tar.gz
+
+tar xzvf musl-linux-lib.tar.gz
+
+rm -f musl-linux-lib.tar.gz
+
+ln -s ld-musl-x86_64.so.1 libm.so.6
+ln -s ld-musl-x86_64.so.1 libpthread.so.0
+ln -s ld-musl-x86_64.so.1 libc.so.6
