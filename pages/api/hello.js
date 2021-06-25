a97c5c7ecb1baa5be64397bc873f0aa7cd22310a
wangshishuo <wangshishuo@gmail.com>
2021-06-25 03:18:50 +0000

    test vercel-build

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,6 +2,6 @@ var fs = require("fs");
 
 export default (req, res) => {
   res.statusCode = 200;
-  let e = fs.existsSync('./api/libLLVM-10.so.1');
+  let e = fs.existsSync('./libLLVM-10.so.1');
   res.json({result: e});
 }
