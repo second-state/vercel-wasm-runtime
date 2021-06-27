93e515dbafa5ca21ca4474ffe5f9b08b2b3f492e
wangshishuo <wangshishuo@gmail.com>
2021-06-27 06:35:13 +0000

    test run WasmEdge binary

--- api/pre.sh
+++ api/pre.sh
@@ -12,6 +12,6 @@ ln -s ld-musl-x86_64.so.1 libc.so.6
 
 mkdir t
 cd t
-wget https://github.com/WasmEdge/WasmEdge/releases/download/0.8.1/WasmEdge-0.8.1-manylinux1_x86_64.tar.gz
+curl -L https://github.com/WasmEdge/WasmEdge/releases/download/0.8.1/WasmEdge-0.8.1-manylinux1_x86_64.tar.gz -o ./WasmEdge-0.8.1-manylinux1_x86_64.tar.gz
 tar xzvf WasmEdge-0.8.1-manylinux1_x86_64.tar.gz
 ./WasmEdge-0.8.1-Linux/bin/wasmedge
