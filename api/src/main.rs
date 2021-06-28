4ddf3615f28c0eb6ee3754dbf2a921b664979ae7
wangshishuo <wangshishuo@gmail.com>
2021-06-28 08:54:28 +0000

    test run WasmEdge binary

---
+++ api/src/main.rs
@@ -0,0 +1,22 @@
+use std::io::{self, Read};
+use image::{ImageOutputFormat, ImageFormat};
+
+fn main() {
+  let mut buf = Vec::new();
+  io::stdin().read_to_end(&mut buf).unwrap();
+
+  let image_format_detected: ImageFormat = image::guess_format(&buf).unwrap();
+  let img = image::load_from_memory(&buf).unwrap();
+  let filtered = img.grayscale();
+  let mut buf = vec![];
+  match image_format_detected {
+    ImageFormat::Gif => {
+        filtered.write_to(&mut buf, ImageOutputFormat::Gif).unwrap();
+    },
+    _ => {
+        filtered.write_to(&mut buf, ImageOutputFormat::Png).unwrap();
+    },
+  }
+  println!("{:?}", buf);
+}
+
