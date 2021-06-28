9a04bb617f48d6f677e0f63639e35a806181a5d5
wangshishuo <wangshishuo@gmail.com>
2021-06-28 11:39:29 +0000

    test run WasmEdge binary

--- api/hello.js
+++ api/hello.js
@@ -7,32 +7,36 @@ module.exports = (req, res) => {
   // res.setHeader('Content-Type', 'application/octet-stream');
   res.setHeader('Content-Type', 'text/plain');
 
-  const wasmedge = spawn(path.join(__dirname, 'WasmEdge-0.8.1-Linux/bin/wasmedge'), ['--dir', `/:${__dirname}`, path.join(__dirname, 'wasi.wasm')]);
+  try {
+    const wasmedge = spawn(path.join(__dirname, 'WasmEdge-0.8.1-Linux/bin/wasmedge'), ['--dir', `/:${__dirname}`, path.join(__dirname, 'wasi.wasm')]);
 
-  let c = 0;
+    let c = 0;
 
-  wasmedge.stdout.on('data', (data) => {
-    // let filePath = new String(data);
-    // res.send(fs.readFileSync(path.join(__dirname, filePath.trim())));
-    res.write(__dirname);
-    if (c == 0) {
-      res.send(data);
-      c = c + 1;
-    }
-  });
+    wasmedge.stdout.on('data', (data) => {
+      // let filePath = new String(data);
+      // res.send(fs.readFileSync(path.join(__dirname, filePath.trim())));
+      res.write(__dirname);
+      if (c == 0) {
+        res.send(data);
+        c = c + 1;
+      }
+    });
 
-  wasmedge.stderr.on('data', (data) => {
-    // let filePath = new String(data);
-    // res.send(fs.readFileSync(path.join(__dirname, filePath.trim())));
-    res.write(__dirname);
-    if (c == 0) {
-      res.send(data);
-      c = c + 1;
-    }
-  });
+    wasmedge.stderr.on('data', (data) => {
+      // let filePath = new String(data);
+      // res.send(fs.readFileSync(path.join(__dirname, filePath.trim())));
+      res.write(__dirname);
+      if (c == 0) {
+        res.send(data);
+        c = c + 1;
+      }
+    });
 
-  let l = fs.readFileSync(path.join(__dirname, 'cowboy.png'));
-  wasmedge.stdin.write(l);
-  wasmedge.stdin.end('');
+    let l = fs.readFileSync(path.join(__dirname, 'cowboy.png'));
+    wasmedge.stdin.write(l);
+    wasmedge.stdin.end('');
+  } catch (e) {
+    res.send(e);
+  }
 }
 
