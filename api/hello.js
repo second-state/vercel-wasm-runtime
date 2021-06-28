17ef800c63b5c3cbcda7f3b54f86300794ce96ce
wangshishuo <wangshishuo@gmail.com>
2021-06-28 11:41:29 +0000

    test run WasmEdge binary

--- api/hello.js
+++ api/hello.js
@@ -17,7 +17,7 @@ module.exports = (req, res) => {
       // res.send(fs.readFileSync(path.join(__dirname, filePath.trim())));
       res.write(__dirname);
       if (c == 0) {
-        res.send(data);
+        // res.send(data);
         c = c + 1;
       }
     });
@@ -27,7 +27,7 @@ module.exports = (req, res) => {
       // res.send(fs.readFileSync(path.join(__dirname, filePath.trim())));
       res.write(__dirname);
       if (c == 0) {
-        res.send(data);
+        // res.send(data);
         c = c + 1;
       }
     });
@@ -38,5 +38,7 @@ module.exports = (req, res) => {
   } catch (e) {
     res.send(e);
   }
+
+  res.send('--');
 }
 
