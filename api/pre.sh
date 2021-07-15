504a837b9d925c8282c4c0e202cd939de98b7f8b
wangshishuo <wangshishuo@gmail.com>
2021-07-15 15:52:26 +0800

    let developer provide .wasm and only compile *.wasm to .so in pre.sh

--- api/pre.sh
+++ api/pre.sh
@@ -10,20 +10,12 @@ curl -L https://github.com/second-state/WasmEdge-tensorflow-deps/releases/downlo
 tar xzvf WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
 rm WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
 
-# install rust
-export HOME=/root
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
-
-rustup default 1.50.0
-rustup target add wasm32-wasi
-
-cd functions/image-classification
-cargo build --target wasm32-wasi --release
-mv target/wasm32-wasi/release/classify.wasm ../..
-cd ../..
-./wasmedgec-tensorflow classify.wasm classify.so
+# compile all .wasm to .so
+for file in *.wasm; do
+    [ -f "$file" ] || continue
+    ./wasmedgec-tensorflow "$file" "${file/.wasm/.so}"
+    rm "$file"
+done
 
 # clean
-rm classify.wasm
 rm wasmedgec-tensorflow
